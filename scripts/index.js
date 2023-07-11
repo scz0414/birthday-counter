83afe6e74a52267be5a85c088efc23e22b8d0441
scz0414 <91317283+scz0414@users.noreply.github.com>
2023-07-11 13:21:05 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Aug 13, 2023',
+  name: 'Ance'
 };
 
 function hideEverything() {
